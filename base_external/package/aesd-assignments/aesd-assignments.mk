8c19a47fa3e93c29ffc0c77be1449da92c9985a4
Tim Bailey <tiba6275@colorado.edu>
2023-10-09 07:49:35 -0400

    Fix aesdsocket initialization

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -28,7 +28,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
