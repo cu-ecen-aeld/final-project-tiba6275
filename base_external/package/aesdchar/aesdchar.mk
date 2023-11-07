0a7262b28360fb4e2e3af30a9adbdba402ff93cb
tiba6275 <tiba6275@colorado.edu>
2023-11-07 18:35:15 -0500

    Update A3 link

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '8c9c6f07983daa50cdafe6a14ac2e58b4d42c111'
+AESDCHAR_VERSION = 'd3a09f7552fb60096099227da1f84ae80d47736e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
